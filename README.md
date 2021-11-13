153d59bfa0ca1e5a830880c33c4171c835355c3d
Peter <psm@acm.org>
2021-11-13 14:04:21 -0800

    polish

--- README.md
+++ README.md
@@ -14,10 +14,31 @@ python -i ./alg-01.py
 Then inside python:
 
 ```
+# reference (Newspaper3K)
+test00()
+# XLNET model
 test01()
 ```
 
 Currently it produces:
+
+```
+“In most cases you’re just making all of your information public in
+one way or another,” said online privacy expert Hayley Kaplan.
+
+“You’re making it so easy for people to know so much about you.”But
+Kaplan says a quick privacy checkup will help protect some of your
+personal information.
+
+And whatever changes you make to your Facebook settings, be sure to
+check them often.
+
+And you can also do a Facebook privacy checkup.
+
+Here’s how to access and change your Google privacy settings.
+```
+
+for the reference and:
 
 ```
 Popular apps and websites, such as Facebook and Amazon, that many of
@@ -31,3 +52,5 @@ make to your Facebook settings, be sure to check them often. Kaplan
 suggests you don’t allow the tech giant to track your browsing
 history. Here’s how to access and change your Google privacy settings.
 ```
+
+for the XLNET model.
--- alg001.py
+++ alg001.py
@@ -1,11 +1,28 @@
 
-from summarizer import TransformerSummarizer
 
 from sample_texts import *
 
-# see also:
-# https://medium.com/analytics-vidhya/text-summarization-using-bert-gpt2-xlnet-5ee80608e961
 
+
+################################################################
+from newspaper import Article, Config
+
+# reference N3K
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0'
+config = Config()
+config.browser_user_agent = user_agent
+def test00(body=TEXT_1, url=URL_1, h_file=HTML_1):
+    a = Article(url, config=config)
+    with open(h_file) as f:
+        h = f.read()
+    a.download(input_html=h)
+    a.parse()
+    a.nlp()
+    return a.summary
+    
+from summarizer import TransformerSummarizer
+# see also:
+#  https://medium.com/analytics-vidhya/text-summarization-using-bert-gpt2-xlnet-5ee80608e961
 XLNet_model = None
 def test01(body=TEXT_1):
     global XLNet_model
@@ -13,3 +30,5 @@ def test01(body=TEXT_1):
         XLNet_model = TransformerSummarizer(transformer_type="XLNet",transformer_model_key="xlnet-base-cased")
     full = ''.join(XLNet_model(body, min_length=60))
     return(full)
+
+
